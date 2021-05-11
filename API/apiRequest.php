46cc3da790c535af9ab4ded99be066c00493c4be
nacachiri <nachocalomarde@gmail.com>
2021-05-11 18:57:44 +0200

    Movido todo a src

--- API/apiRequest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $return = file_get_contents("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroProvincia/46");
-    var_dump(json_decode($return));
-    
-?>
