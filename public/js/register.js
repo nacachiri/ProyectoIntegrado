10527d642cf7d9c7b568dfa977505cf691703cfe
nacachiri <nachocalomarde@gmail.com>
2021-05-13 18:52:02 +0200

    Acabado registro usuarios

--- public/js/register.js
+++ public/js/register.js
@@ -36,15 +36,16 @@ document.addEventListener("DOMContentLoaded", () => {
                     errorStrong.innerHTML = 'OK!'
                     spanMensajeAlerta.innerHTML = 'Se ha registrado correctamente';
 
-                    setInterval(() => {
+                    setTimeout(() => {
                         window.location.href = 'index.php';
-                    }, 3000);
+                    }, 2500);
                 
                 }else{
 
                     let divAlerta = document.getElementById('divAlerta');
                     let spanMensajeAlerta = document.getElementById('divMensajeError');
                     divAlerta.classList.remove('hidden');
+                    divAlerta.className = 'border text-red-700 px-4 py-3 rounded relative bg-red-100 border-red-400';
                     spanMensajeAlerta.innerHTML = response.data;
 
                 }
@@ -56,6 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
             let divAlerta = document.getElementById('divAlerta');
             let spanMensajeAlerta = document.getElementById('divMensajeError');
             divAlerta.classList.remove('hidden');
+            divAlerta.className = 'border text-red-700 px-4 py-3 rounded relative bg-red-100 border-red-400';
             spanMensajeAlerta.innerHTML = 'Alguno de los campos esta vacio';
 
         }
